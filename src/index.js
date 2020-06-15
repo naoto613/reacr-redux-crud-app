1155bff7c372ca34d9543448278d09a9548360c8
naoto <you@example.com>
2020-06-15 09:07:15 +0900

    createevent and getevent

--- src/index.js
+++ src/index.js
@@ -2,20 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { createStore} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
+import thunk from 'redux-thunk'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import App from './components/App';
+import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+    <Switch>
+      <Route exact path="/events/new" component={EventsNew} />
+      <Route exact path="/" component={EventsIndex} />
+    </Switch>
+    </BrowserRouter>
   </Provider>,
-
   document.getElementById('root')
 );
 
